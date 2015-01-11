068eb3269aa06e39912005f5cf1b892580cbcbb3
Tyrfingr <bartuni38@googlemail.com>
2015-01-11 21:57:41 +0100

    Version 0.1.1

--- mensa/spiders/mensa_spider.py
+++ mensa/spiders/mensa_spider.py
@@ -1,17 +1,34 @@
 import scrapy
-
+from  mensa.items import Hauptgericht, Beilage 
 class MensaSpider(scrapy.Spider):
     name = "mensa"
     allowed_domains = ["stw-on.de"]
     start_urls = ["http://www.stw-on.de/braunschweig/essen/menus/mensa-1"]
 
     def parse(self, response):
-        #meal = response.xpath("//table[@id='*']").extract()
-        for sel in response.xpath("//table"):
-            for zel in sel.xpath(".//td[@class='swbs_speiseplan_meal']/text()[1]"):
-                meal = zel.extract()
-                print meal 
-            for price in sel.xpath(".//td[@class='swbs_speiseplan_price_s']/text()"):
-                price_meal = price.extract()
-                print price_meal
-
+        for table in response.xpath("//table"):
+            haupt = Hauptgericht()
+            beil = Beilage()
+            haupt ["day"] = table.xpath("./tr/th[@colspan='2']/text()").extract()
+            beil ["day"] = haupt["day"]
+            for tr in table.xpath(".//tr"): 
+                
+                for td in tr.xpath(".//td[@class='swbs_speiseplan_meal']/text()"):
+                    haupt["meal"]  = td.extract()
+                    
+                    haupt["price_s"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    
+                    haupt["price_g"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    
+                    haupt["price_e"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    yield haupt
+                
+                for td in tr.xpath(".//td[@class='swbs_speiseplan_other']/text()"):
+                    beil["other"] = td.extract()
+                    
+                    beil["price_s"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    
+                    beil["price_g"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    
+                    beil["price_e"] = tr.xpath(".//td[@class='swbs_speiseplan_price_s']/text()").extract()
+                    yield beil
