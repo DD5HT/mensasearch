c4c8fed66878fda70883b749a3c0c2f9c1eb28a8
Tim Schubert <tim.schubert94@gmail.com>
2015-03-29 18:03:44 +0200

    added spider for item lable explanation

--- mensa/settings.py
+++ mensa/settings.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for mensa project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
-BOT_NAME = 'mensa'
+BOT_NAME = 'mensasearch'
 
 SPIDER_MODULES = ['mensa.spiders']
 NEWSPIDER_MODULE = 'mensa.spiders'
 
 ITEM_PIPELINES = {
-        'mensa.save_to_db.MensaPipeline': 900,
+        'mensa.pipelines.MealPipeline': 900,
+        'mensa.pipelines.LablePipeline': 1000,
 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
